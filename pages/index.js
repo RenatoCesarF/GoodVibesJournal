964dad0df32bcb85c3b8e10b40df7ae71b6a2a81
Renato <re.fbarcellos@hotmail.com>
2021-01-21 12:54:02 -0400

    :lipstick: Trying the fist frontend stuffs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,48 @@
-function Home() {
+import styles from"./styles/index.module.css"
 
-    return <div>
-        <h3>Home</h3>
+function getDayFase() {
+    var dayFase;
+    var time = new Date().getHours();
+    
+    
+    if (time < 12) {
+        dayFase = "Morning";
+    } else if (time => 12 & time < 18) {
+        dayFase = "Evening";
+    } else {
+        dayFase = "Night";
+    }
+    
+    return dayFase;
+}
+
+function Home({ Component, pageProps }) {    
+    var dayFase = getDayFase();
+
+    return (
+        <div>
+
+            <style jsx global >{`
+                HTML,BODY{
+                    background: linear-gradient(180deg, #6D6875 1.9%, rgba(181, 131, 141, 0.862522) 67.42%, rgba(229, 152, 155, 0.656673) 88.67%, rgba(255, 180, 162, 0.353189) 100%), #E5989B;
+
+                    margin: 0;
+                    padding: 0;
+                    background-size:100%;
+                    
+                    width: 100%;
+                    height:100%;
+
+                    overflow: hidden;
+                    
+                }
+            `}
+            </style>
+
+                <h1 className={styles.title} >Good <br/>{dayFase}...</h1>
+           
         </div>
+    );
 }
 
 export default Home
