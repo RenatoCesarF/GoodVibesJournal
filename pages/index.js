8d00de7113e8058d8a459180f8996cce32853adf
Renato <re.fbarcellos@hotmail.com>
2021-01-21 14:56:35 -0400

    :dizzy: Donw buttom + it's animation

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styles from"./styles/index.module.css"
+import { FaAngleDoubleDown} from 'react-icons/fa';
 
 function getDayFase() {
     var dayFase;
@@ -23,7 +24,9 @@ function Home({ Component, pageProps }) {
         <div>
 
             <style jsx global >{`
-                HTML,BODY{
+    
+            
+            HTML,BODY{
                     background: linear-gradient(180deg, #6D6875 1.9%, rgba(181, 131, 141, 0.862522) 67.42%, rgba(229, 152, 155, 0.656673) 88.67%, rgba(255, 180, 162, 0.353189) 100%), #E5989B;
 
                     margin: 0;
@@ -38,11 +41,16 @@ function Home({ Component, pageProps }) {
                 }
             `}
             </style>
-
+            <div>
                 <h1 className={styles.title} >Good <br/>{dayFase}...</h1>
+            </div>
+            
+       
+                <FaAngleDoubleDown className={styles.icon}/>
            
-        </div>
+         
+        </div>  
     );
 }
 
-export default Home
+export default Home;
