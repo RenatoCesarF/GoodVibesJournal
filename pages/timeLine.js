7be651aa3b2ab6ae840220369230f80cc39db87b
Renato <re.fbarcellos@hotmail.com>
2021-01-23 11:08:32 -0400

    :art: Improving file system organization structure

--- pages/timeLine.js
+++ pages/timeLine.js
@@ -1,28 +1,39 @@
-import { getDayFase, getDayColor } from './index' 
+import { render } from "react-dom";
+import React,{ useState } from 'react'
 
+import dynamic from 'next/dynamic';
+import Link  from 'next/link';
+
+import { getDayFase, getDayColor } from '../utils/dayMomentSystem';
+import styles from '../../styles/timeLine.module.css';
+
+import Header from '../components/Header/Header'
 
 function timeLine({ Component, pageProps }) {   
     
-    var dayFase = getDayFase()
+    var dayFase = getDayFase();
 
     return (
         <div>
-        <style jsx global >{`       
-            HTML,BODY{
-                    ${getDayColor(dayFase)}
-                    margin: 0;
-                    padding: 0;
-                    background-size:100%;
-                    
-                    width: 100%;
-                    height:100%;
 
-                    overflow: hidden;
-                    
-                }
-            `}
-              </style>
- 
+            <style jsx global >{`       
+                HTML,BODY{
+                        ${getDayColor(dayFase)}
+                        margin: 0;
+                        padding: 0;
+                        background-size:100%;
+                        
+                        width: 100%;
+                        height:100%;
+
+                        overflow: hidden;
+                        
+                    }
+                `}
+                </style>
+
+                <Header/>
+           
         </div>  
     );
 }
---
+++ utils/dayMomentSystem.js
@@ -0,0 +1,30 @@
+export function getDayFase() {
+    var dayFase;
+    var time = new Date().getHours();
+    
+    
+    if (time < 12) {
+        dayFase = "Morning";
+    } else if (time >= 12 & time < 18) {
+        dayFase = "Evening";
+    } else {
+        dayFase = "Night";
+    }
+    
+    return dayFase;
+}
+
+export function getDayColor(dayFase){
+    var backgroundColor;
+    
+    if (dayFase == "Morning") {
+        backgroundColor = "background: linear-gradient(180deg, #6D6875 1.9%, rgba(181, 131, 141, 0.862522) 67.42%, rgba(229, 152, 155, 0.656673) 88.67%, rgba(255, 180, 162, 0.353189) 100%), #E5989B;"
+    }
+    else if (dayFase == "Evening") {
+        backgroundColor = "background: linear-gradient(180deg, #d69265 20.57%, #d9bb59 71.05%, rgba(255, 239, 179, 0.353189) 100%), #F8DE7E;"
+    }
+    else if(dayFase == "Night"){
+        backgroundColor = "background: linear-gradient(180deg, #0F142D 0%, #3B4974 52.71%, #6192B1 100%), #0A2472;"
+    }
+    return backgroundColor;
+}
