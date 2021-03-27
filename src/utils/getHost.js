65fc156f4a8d1742100e47f910b3e3576969542f
Renato <re.fbarcelloss@hotmail.com>
2021-03-27 19:53:16 -0400

    :pencil2: Fix typo

--- src/utils/getHost.js
+++ src/utils/getHost.js
@@ -1,5 +1,5 @@
 export default function getHost() {
-    var host = 'http://localhost:3000'//"https://goodvibesjornal.vercel.app"
+    var host = "https://goodvibesjornal.vercel.app"
 
     return host
 }
