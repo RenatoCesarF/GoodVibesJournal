c7a71bbd181dc78e579d9eb860079915e67ec958
Renato <re.fbarcelloss@hotmail.com>
2021-03-27 20:08:20 -0400

    :bug: Deploy fail bug repair try

---
+++ src/pages/timeLine.js
@@ -0,0 +1,53 @@
+import React,{ Component } from 'react'
+import Head from 'next/head'
+
+import { getDayColor } from '../utils/dayMomentSystem';
+import ListItens from '../utils/menuItems'
+
+import Header from '../components/Header/Header';
+import Menu from '../components/Menu/Menu';
+import Scroll from '../components/Scroll/Scroll';
+
+export default class timeLine extends Component {
+    state = {
+        option: ListItens[2],
+    }
+
+    listenMenu = (selectedOption) => {
+        this.setState({ option: selectedOption })
+    }
+   
+    render() {
+        return (
+            <div>
+                
+                <Head>
+                    <link rel="shortcut icon" href='../../static/icon_unfilled.ico'/>
+                    <title>Good Vibes Jornal</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                </Head>
+ 
+                <style jsx global >{`       
+                html,body{
+                        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+                        ${getDayColor()}
+                        margin: 0;
+                        padding: 0;
+                        background-size:100%;
+                        
+                        width: 100vw;
+                        height:100vh;
+                        
+                        overflow: hidden;
+                    }
+                `}
+                </style>
+                    
+                <Header/>
+                <Menu listenMenu={this.listenMenu}/>
+                <Scroll timelineOption={this.state.option}/>
+            </div>
+        );
+    }
+}
